d5b3873c477780c2df01d5d80910f011e9c4fa8d
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-10 18:03:00 +0100

    references: add os and url modules references

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -7,20 +7,36 @@ const path = require('path');
 // 	console.log('Folder created...');
 // }); // asynchronous by default, takes a callback
 
-// Create and write to file
-fs.writeFile(
-	path.join(__dirname, '/test', 'hello.txt'), 
-	'Hello World!',
-    err => {
-		if (err) throw err;
-		console.log('File written to');
 
-		// Append to file without overdifing
-		fs.appendFile(
-			path.join(__dirname, '/test', 'hello.txt'), 
-			' Yes!',
-		    err => {
-				if (err) throw err;
-				console.log('File written to');
-		});
-});
+// Create and write to file
+// second parameter is content you want to write
+// fs.writeFile(
+// 	path.join(__dirname, '/test', 'hello.txt'), 
+// 	'Hello World!',
+//     err => {
+// 		if (err) throw err;
+// 		console.log('File written to');
+
+// 		// Append to file without overdifing
+// 		fs.appendFile(
+// 			path.join(__dirname, '/test', 'hello.txt'), 
+// 			' Yes!',
+// 		    err => {
+// 				if (err) throw err;
+// 				console.log('File written to');
+// 		});
+// });
+
+// Read
+// Second parameter is encoding
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+// 	if (err) throw err;
+// 	console.log(data);
+// }); 
+
+
+// Rename file
+// fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'hellobob.txt'), (err) => {
+// 	if (err) throw err;
+// 	console.log("File renamed.");
+// }); 
